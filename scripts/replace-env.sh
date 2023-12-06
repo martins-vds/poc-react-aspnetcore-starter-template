e411397d4673d7370d074f542587ce24f78784e1
Vinny Martins <martins.vds@outlook.com>
2023-12-06 14:37:39 -0700

    Fix case-insensitive environment variable replacement

--- scripts/replace-env.sh
+++ scripts/replace-env.sh
@@ -8,5 +8,6 @@ directory="$3"
 
 # Find js files in the directory and replace the environment variables
 find $directory -type f -name "*.js" -print0 | while IFS= read -r -d '' file; do
-    sed -i "s|$replace|$with|g" $file
+    # case insensitive replace
+    sed -i "s|$replace|$with|gI" $file
 done
